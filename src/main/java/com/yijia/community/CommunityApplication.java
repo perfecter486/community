001937e881d812604ec210ad2afda347145ea831
yijia <yijia.com>
2021-07-27 17:54:57 +0800

    first commit

---
+++ src/main/java/com/yijia/community/CommunityApplication.java
@@ -0,0 +1,17 @@
+package com.yijia.community;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.ui.Model;
+import org.springframework.ui.ModelMap;
+
+@SpringBootApplication
+public class CommunityApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CommunityApplication.class, args);
+
+
+    }
+
+}
